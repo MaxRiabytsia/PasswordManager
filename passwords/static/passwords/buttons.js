7d9133006d3c98ccf93f4d656915b9c1c66a2882
Max <riabytsa@gmail.com>
2022-09-13 16:35:44 +0300

    Added dark theme

--- passwords/static/passwords/buttons.js
+++ passwords/static/passwords/buttons.js
@@ -1,10 +1,8 @@
 function showPassword(event, password) {
-    console.log(event.target.innerHTML)
     if (event.target.innerHTML.trim() === 'Show') {
         event.target.closest('#password_row').querySelector('#password').innerHTML = password;
         event.target.innerHTML = 'Hide';
-    }
-    else {
+    } else {
         event.target.closest('#password_row').querySelector('#password').innerHTML = "•••••••••••";
         event.target.innerHTML = 'Show';
     }
@@ -12,5 +10,9 @@ function showPassword(event, password) {
 
 
 function copy(text) {
+    document.getElementById("custom-tooltip").style.display = "inline";
     navigator.clipboard.writeText(text);
+    setTimeout( function() {
+        document.getElementById("custom-tooltip").style.display = "none";
+    }, 1000);
 }
--- users/models.py
+++ users/models.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    preferred_theme = models.CharField(max_length=255, default='light', choices=[('light', 'Light Theme'),
+                                                                                 ('dark', "Dark Theme")
+                                                                                 ])
 
     def __str__(self):
         return f"{self.user.username} Profile"
