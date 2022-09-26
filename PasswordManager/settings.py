59f6a7d7220446ae49b57ac02d01fc289d1e7200
Max <riabytsa@gmail.com>
2022-09-26 10:58:17 +0300

    Changed secret key

--- PasswordManager/settings.py
+++ PasswordManager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fiojf($ckj#)fkx(@2lflfj93#)3kss;3k2p(&jd2'
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
