cae02c400599059bc1be8109e5256c8d5ad61d20
Max <riabytsa@gmail.com>
2022-10-03 11:50:39 +0300

    Improved encryption system

--- passwords/models.py
+++ passwords/models.py
@@ -1,26 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import DEFERRED
 from django.utils import timezone
 from django.urls import reverse
-from django_cryptography.fields import encrypt
 
-from passlib.hash import pbkdf2_sha256
-
+import cryptocode
 
 DEFAULT_ICON = "https://w7.pngwing.com/pngs/285/477/png-transparent-web-development-web-design-internet-" \
                "web-hosting-service-world-wide-web-logo-symmetry-sphere.png"
+ENCRYPTED_FIELDNAMES = ["service_icon_url", "service_name", "service_url", "password",
+                        "username", "email", "additional_info"]
 
 
 class Password(models.Model):
-    user_key = bytes(pbkdf2_sha256.hash(f"{User.pk}{User.password}{User.email}", rounds=200000, salt_size=32)[:32],
-                     'utf-8')
-    service_url = encrypt(models.URLField(max_length=255, default=''), key=user_key)
-    service_icon_url = encrypt(models.URLField(max_length=255, default=DEFAULT_ICON), key=user_key)
-    service_name = encrypt(models.CharField(max_length=255, default=''), key=user_key)
-    password = encrypt(models.CharField(max_length=255, default=''), key=user_key)
-    username = encrypt(models.CharField(max_length=255, blank=True, default=''), key=user_key)
-    email = encrypt(models.EmailField(max_length=255, blank=True, default=''), key=user_key)
-    additional_info = encrypt(models.TextField(blank=True, default=''), key=user_key)
+    service_url = models.URLField(max_length=255, default='')
+    service_icon_url = models.URLField(max_length=255, default=DEFAULT_ICON)
+    service_name = models.CharField(max_length=255, default='')
+    password = models.CharField(max_length=255, default='')
+    username = models.CharField(max_length=255, blank=True, default='')
+    email = models.EmailField(max_length=255, blank=True, default='')
+    additional_info = models.TextField(blank=True, default='')
     date_created = models.DateTimeField(default=timezone.now)
     last_modified = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
@@ -29,9 +28,38 @@ class Password(models.Model):
         return f"Password for {self.service_url} created by {self.author}"
 
     def save(self, *args, **kwargs):
-        self.service_icon_url = self.get_icon_url(self.service_url)
-        self.service_name = self.get_service_name(self.service_url)
+        master_password = f"{self.author.pk}{self.author.password}"
+        print(master_password)
+
+        self.service_icon_url = cryptocode.encrypt(self.get_icon_url(self.service_url), master_password)
+        self.service_name = cryptocode.encrypt(self.get_service_name(self.service_url), master_password)
+        self.service_url = cryptocode.encrypt(self.service_url, master_password)
+        self.password = cryptocode.encrypt(self.password, master_password)
+        self.username = cryptocode.encrypt(self.username, master_password)
+        self.email = cryptocode.encrypt(self.email, master_password)
+        self.additional_info = cryptocode.encrypt(self.additional_info, master_password)
+
         super(Password, self).save(*args, **kwargs)
+
+    @classmethod
+    def from_db(cls, db, field_names, values):
+        if len(values) != len(cls._meta.concrete_fields):
+            values_iter = iter(values)
+            values = [
+                next(values_iter) if f.attname in field_names else DEFERRED
+                for f in cls._meta.concrete_fields
+            ]
+
+        user = User.objects.get(id=values[-1])
+        master_password = f"{user.pk}{user.password}"
+        for i, (fieldname, val) in enumerate(zip(field_names, values)):
+            if fieldname in ENCRYPTED_FIELDNAMES:
+                values[i] = cryptocode.decrypt(val, master_password)
+
+        new = cls(*values)
+        new._state.adding = False
+        new._state.db = db
+        return new
 
     @staticmethod
     def get_icon_url(service_url):
@@ -42,11 +70,10 @@ class Password(models.Model):
     @staticmethod
     def get_service_name(service_url):
         service_name = service_url.replace("https://", '').replace("http://", '').replace("www.", '')
-        last_dot_index = -service_name[::-1].index('.') - 1
-        service_name = service_name[:last_dot_index]
+        domain_name_start_index = -service_name[::-1].index('.') - 1
+        service_name = service_name[:domain_name_start_index]
         service_name = service_name.capitalize()
         return service_name
 
     def get_absolute_url(self):
         return reverse('password-detail', kwargs={'pk': self.pk})
-
